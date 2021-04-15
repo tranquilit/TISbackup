0fc9f050e524d5a0ea232f35b3c6a7f6437e424b
Denis Cardon <dcardon@tranquil.it>
2021-04-15 15:24:00 +0200

    cleanup sphinx doc for github.io

--- doc/make_doc.sh
+++ doc/make_doc.sh
@@ -9,34 +9,12 @@ make clean
 
 rm -Rf ./build/
 
-echo "gettext and update po"
-make gettext
-sphinx-intl update -p build/locale/ -l fr
-
 make clean
 
 echo "make html English"
 make htmlen
 
-echo "make html French"
-make -e SPHINXOPTS="-D language='fr'" htmlfr
-
-echo "make epub EN"
-make epub_en
-
-echo "make epub FR"
-make -e SPHINXOPTS="-D language='fr'" epub_fr
-
-echo "make latexpdf EN"
-# we ignore errors from that build for now
-make latexpdf_en  || true
-
-echo "make latexpdf FR"
-make -e SPHINXOPTS="-D language='fr'" latexpdf_fr || true
-
 cp ./robots.txt build/en/doc
-cp ./robots.txt build/fr/doc
 mkdir ./build/en/doc/.well-known
-mkdir ./build/fr/doc/.well-known
 cp security.txt ./build/en/doc/.well-known
-cp security.txt ./build/en/doc/.well-known
+touch ./build/en/doc/.nojekyll
